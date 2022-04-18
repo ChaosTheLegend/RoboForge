d23bc13fc2a2db60934039b533ff3a857cc6286d
ChaosTheLegend <chaosthelegend@inbox.ru>
2022-04-18 21:10:34 +0600

    Fixed player rotation to aim
    still need to make new rotation for weapons

--- RobotForge/Assets/NetworkCharacterOrientation3D.cs
+++ RobotForge/Assets/NetworkCharacterOrientation3D.cs
@@ -1,35 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
 
 public class NetworkCharacterOrientation3D : CharacterAbility
 {
     [SerializeField] private GameObject ModelToRotate;
-
+    [SerializeField] private float RotationSpeed = 10f;
+    [SerializeField] private RotationMode rotationMode;
     private Quaternion _lastRotation;
     
-    public override void FixedUpdateNetwork()
+    private CharacterHandleWeapon _characterHandleWeapon;
+    
+    [Networked] private  Quaternion _targetRotation { get; set; }
+    
+    private Vector3 _aim;
+    private RotationMode currentRotationMode;
+    private enum RotationMode
     {
-        RotateToFaceMovement();
+        None,
+        FaceMovement,
+        FaceWeapon,
+        Both
+    }
+    public override void Spawned()
+    {
+        _characterHandleWeapon = GetComponent<CharacterHandleWeapon>();
     }
 
-
-    //TODO : implement this
+    public override void FixedUpdateNetwork()
+    {
+        if(rotationMode == RotationMode.None) return;
+        
+        if (ModelToRotate == null) return;
+        if(_controller3D == null) return;
+        
+        currentRotationMode = RotationMode.None;
+        
+        //Character won't rotate to face weapon if it's not equipped
+        if(rotationMode is RotationMode.FaceWeapon or RotationMode.Both) RotateToFaceWeapon();
+        if(currentRotationMode == RotationMode.FaceWeapon) return;
+        
+        //Check if character has a weapon
+        if(rotationMode is RotationMode.FaceMovement or RotationMode.Both) RotateToFaceMovement();
+        
+    }
+    
     private void RotateToFaceWeapon()
     {
+        if(_characterHandleWeapon == null) return;
+        var weapon = _characterHandleWeapon.CurrentWeapon;
+        if (weapon == null) return;
         
+        //Smoothly rotate towards the aim
+        var aim = _characterHandleWeapon.WeaponAimComponent.CurrentAim;
+        aim.y = 0;
+        _targetRotation = Quaternion.LookRotation(aim);
+        ModelToRotate.transform.rotation = Quaternion.Slerp(ModelToRotate.transform.rotation, _targetRotation, RunnerDeltaTime * RotationSpeed);
+        currentRotationMode = RotationMode.FaceWeapon;
     }
 
     
     //Rotate the model to face movement direction
     private void RotateToFaceMovement()
     {
-        if (ModelToRotate == null) return;
-        if(_controller3D == null) return;
         if (_controller3D.Velocity.sqrMagnitude < 1f)
         {
-            ModelToRotate.transform.rotation =  Quaternion.Lerp(ModelToRotate.transform.rotation, _lastRotation, RunnerDeltaTime * 10f);
+            ModelToRotate.transform.rotation =  Quaternion.Slerp(ModelToRotate.transform.rotation, _lastRotation, RunnerDeltaTime * RotationSpeed);
             return;
         }
         
@@ -44,7 +83,9 @@ public class NetworkCharacterOrientation3D : CharacterAbility
             Quaternion.Lerp(rotation, targetRotation, RunnerDeltaTime * 10f);
         ModelToRotate.transform.rotation = rotation;
         _lastRotation = rotation;
+
+        currentRotationMode = RotationMode.FaceMovement;
     }
-    
-    
 }
+
+
